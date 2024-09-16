2a46722e4b113cb569b54f384bb503d447dfbac4
pkhead <chromosoze@gmail.com>
2024-09-16 17:45:19 -0500

    overhauled type system

--- astgen.py
+++ astgen.py
@@ -1,11 +1,52 @@
 from lexer import Token
 from compileerror import CompilationException
 
+class ValueType:
+    VOID = 0
+    NUMBER = 1
+    STRING = 2
+    BOOL = 3
+
+    # TODO: pointers
+    POINTER = 4
+
+    def __init__(self, type, base_type=None):
+        self.type = type
+        self.base_type = base_type
+    
+    @staticmethod
+    def pointer_to(base_type):
+        return ValueType(ValueType.POINTER, base_type)
+    
+    def __str__(self):
+        if self.type == ValueType.POINTER:
+            return str(self.base_type) + "*"
+        if self.type == ValueType.NUMBER:
+            return "number"
+        elif self.type == ValueType.STRING:
+            return "string"
+        elif self.type == ValueType.BOOL:
+            return "bool"
+        elif self.type == ValueType.VOID:
+            return "void"
+        else:
+            raise Exception("internal: unknown ValueType value " + str(self.type))
+    
+    def is_a(self, type_id):
+        return self.type == type_id
+    
+    def is_same(self, other):
+        if self.type == ValueType.POINTER and other.type == ValueType.POINTER:
+            return self.base_type.is_same(other.base_type)
+        else:
+            return self.type == other.type
+    
 class Block:
     def __init__(self, parent=None):
         self.variables = {}
         self.parent = parent
         self.statements = []
+        self.return_type = None
     
     def declare_variable(self, var_name, var_type):
         self.variables[var_name] = {
@@ -18,28 +59,41 @@ class Block:
             return self.variables[var_name]
         
         return None if self.parent == None else self.parent.get_variable_info(var_name)
+
+class ExpressionAtom:
+    def __init__(self, scope, tok):
+        self.token = tok
+        
+        if tok.type == Token.TYPE_NUMBER:
+            self.type = ValueType(ValueType.NUMBER)
+        elif tok.type == Token.TYPE_STRING:
+            self.type = ValueType(ValueType.STRING)
+        elif tok.type == Token.TYPE_IDENTIFIER:
+            self.type = scope.get_variable_info(tok.value).type
     
 class BinaryOperator:
-    def __init__(self, type, a, b):
+    def __init__(self, op, type, a, b):
+        self.op = op
         self.type = type
         self.left = a
         self.right = b
     
     def __str__(self):
-        return f'{(self.type)}({str(self.left)}, {str(self.right)})'
+        return f'{(self.op)}({str(self.left)}, {str(self.right)})'
 
 class UnaryOperator:
-    def __init__(self, type, expr):
+    def __init__(self, op, type, expr):
+        self.op = op
         self.type = type
         self.expr = expr
     
     def __str__(self):
-        return f'{(self.type)}({str(self.expr)})'
+        return f'{(self.op)}({str(self.expr)})'
 
 class IdentifierOperator:
-    def __init__(self, type, output_type, id, data=None):
+    def __init__(self, op, type, id, data=None):
+        self.op = op
         self.type = type
-        self.output_type = output_type
         self.id = id
         self.data = data
     
@@ -68,7 +122,7 @@ def parse_expression(program, tokens, block, order=0):
     # order 1: and
     # order 2: == !=
     # order 3: < > <= >=
-    # order 4: ..
+    # order 4: & (string concatenation)
     # order 5: + -
     # order 6: * / %
     # order 7: not - (unary)
@@ -86,9 +140,12 @@ def parse_expression(program, tokens, block, order=0):
         op_tok = tokens.peek()
         while op_tok.is_symbol('+') or op_tok.is_symbol('-'):
             op_name = ARITHMETIC_SYMBOLS_NAMES[ARITHMETIC_SYMBOLS.index(tokens.pop().value)]
-
             b = parse_expression(program, tokens, block, next_order)
-            a = BinaryOperator(op_name, a, b)
+
+            if not (a.type.is_a(ValueType.NUMBER) and b.type.is_a(ValueType.NUMBER)):
+                raise CompilationException.from_token(op_tok, "attempt to add " + str(a.type) + " with " + str(b.type))
+            
+            a = BinaryOperator(op_name, ValueType(ValueType.NUMBER), a, b)
             op_tok = tokens.peek()
         
         return a
@@ -98,9 +155,12 @@ def parse_expression(program, tokens, block, order=0):
         op_tok = tokens.peek()
         while op_tok.is_symbol('*') or op_tok.is_symbol('/'):
             op_name = ARITHMETIC_SYMBOLS_NAMES[ARITHMETIC_SYMBOLS.index(tokens.pop().value)]
-
             b = parse_expression(program, tokens, block, next_order)
-            a = BinaryOperator(op_name, a, b)
+            
+            if not (a.type.is_a(ValueType.NUMBER) and b.type.is_a(ValueType.NUMBER)):
+                raise CompilationException.from_token(op_tok, "attempt to multiply " + str(a.type) + " with " + str(b.type))
+
+            a = BinaryOperator(op_name, ValueType(ValueType.NUMBER), a, b)
             op_tok = tokens.peek()
         
         return a
@@ -143,9 +203,36 @@ def parse_expression(program, tokens, block, order=0):
         
         elif tok.type == Token.TYPE_STRING or tok.type == Token.TYPE_NUMBER:
             tokens.pop()
-            return tok
+            return ExpressionAtom(block, tok)
     
     raise Exception('parse_expression: unreachable code')
+
+def parse_type(program, tokens, allow_void=False):
+    tok = tokens.pop()
+    if tok.type != Token.TYPE_KEYWORD:
+        raise CompilationException.from_token(tok, "expected type, got " + str(tok))
+    
+    if tok.value == 'number':
+        type = ValueType(ValueType.NUMBER)
+    elif tok.value == 'string':
+        type = ValueType(ValueType.STRING)
+    elif tok.value == 'bool':
+        type = ValueType(ValueType.BOOL)
+    elif tok.value == 'void':
+        type = ValueType(ValueType.VOID)
+    else:
+        raise CompilationException.from_token(tok, "expected type, got " + str(tok))
+    
+    # accumulate pointer types
+    while tokens.peek().is_symbol('*'):
+        tokens.pop()
+        type = ValueType.pointer_to(type)
+    
+    if not allow_void and type.type == ValueType.VOID:
+        raise CompilationException.from_token(tok, "void type not allowed")
+
+    return type
+    
 
 def parse_block(program, tokens, parent_block=None):
     block = Block(parent_block)
@@ -160,14 +247,15 @@ def parse_block(program, tokens, parent_block=None):
         elif tok.is_keyword('var'):
             var_name = tokens.pop().get_identifier()
             assert(tokens.pop().is_symbol(':'))
-            var_type_token = tokens.pop()
-            assert(var_type_token.type == Token.TYPE_KEYWORD and var_type_token.value in Token.KEYWORD_TYPES)
-            var_type = var_type_token.value
+            var_type = parse_type(program, tokens)
 
             # variable declaration + assignment in one line
-            if tokens.peek().is_symbol('='):
+            tok = tokens.peek()
+            if tok.is_symbol('='):
                 tokens.pop()
                 expr = parse_expression(program, tokens, block)
+                if not expr.type.is_same(var_type):
+                    raise CompilationException.from_token(tok, f"attempt to assign a {str(expr.type)} to a {str(var_type)}")
                 statement = {
                     'type': 'var_declare',
                     'var_name': var_name,
@@ -195,6 +283,7 @@ def parse_block(program, tokens, parent_block=None):
 
 def parse_function(program, tokens, name, func_type, attribs):
     block = parse_block(program, tokens)
+    block.return_type = func_type
     return Function(name, func_type, block, attribs[:])
 
 def parse_program(tokens):
@@ -240,12 +329,10 @@ def parse_program(tokens):
             assert(tokens.pop().is_symbol(')'))
             assert(tokens.pop().is_symbol(':'))
 
-            func_type = tokens.pop()
-            assert(func_type.type == Token.TYPE_KEYWORD and func_type.value in Token.KEYWORD_TYPES)
-
+            func_type = parse_type(program, tokens, True)
             program['functions'][func_name] = parse_function(
                 program, tokens,
-                name=func_name, func_type=func_type.value, attribs=attributes)
+                name=func_name, func_type=func_type, attribs=attributes)
 
             attributes.clear()
         
