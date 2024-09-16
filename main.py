f02bb17226d4f84f556dea17007f7435454f5424
pkhead <chromosoze@gmail.com>
2024-09-15 22:54:14 -0500

    toplevel program parsing

--- main.py
+++ main.py
@@ -1,27 +1,3 @@
-KEYWORDS = [
-    # asset links
-    'costume', 'sound',
-
-    # types
-    'void', 'string', 'number',
-
-    # events
-    'on', 'flag', 'keypress', 'click', 'broadcast'
-]
-
-SYMBOLS = [
-    ';',
-    '{', '}', '(', ')', '[', ']',
-    ':', ',', '"', '\'',
-    '=',
-
-    # operators
-    '+', '-', '*', '/', '..'
-]
-
-NUMERIC_CHARS = ['.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-WORD_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_"
-
 TOKEN_KEYWORD = 0
 TOKEN_SYMBOL = 1 
 TOKEN_IDENTIFIER = 2
@@ -32,8 +8,79 @@ class Token:
     def __init__(self, token_type, value):
         self.type = token_type
         self.value = value
+    
+    def __str__(self):
+        if self.type == TOKEN_KEYWORD:
+            kw_str = "keyword"
+        elif self.type == TOKEN_SYMBOL:
+            kw_str = "symbol"
+        elif self.type == TOKEN_IDENTIFIER:
+            kw_str = "identifier"
+        elif self.type == TOKEN_NUMBER:
+            kw_str = "number"
+        elif self.type == TOKEN_STRING:
+            kw_str = "string"
+        else:
+            raise Exception("unknown token type")
+            
+        return f"<{kw_str} {(self.value)}>"
+    
+    def is_keyword(self, kw):
+        return self.type == TOKEN_KEYWORD and self.value == kw
+    
+    def is_symbol(self, sym):
+        return self.type == TOKEN_SYMBOL and self.value == sym
+    
+    def get_identifier(self):
+        if self.type != TOKEN_IDENTIFIER:
+            raise Exception('expected identifier')
+        return self.value
+
+class TokenQueue:
+    def __init__(self, p_list):
+        self.list = p_list
+        self.index = 0
+    
+    def pop(self):
+        value = self.list[self.index]
+        self.index += 1
+        return value
+    
+    def peek(self):
+        return self.list[self.index]
+    
+    def __bool__(self):
+        return self.index < len(self.list)   
 
 def parse_tokens(file_path):
+    KEYWORDS = [
+        'func', 'var',
+
+        'if', 'while', 'for', 'block', 'drop', 'end',
+
+        # asset links
+        'costume', 'sound',
+
+        # types
+        'void', 'string', 'number',
+
+        # events
+        'on'
+    ]
+
+    SYMBOLS = [
+        ';',
+        '{', '}', '(', ')', '[', ']',
+        ':', ',', '"', '\'',
+        '=',
+
+        # operators
+        '+', '-', '*', '/', '..'
+    ]
+
+    NUMERIC_CHARS = ['.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    WORD_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_"
+
     tokens = []
 
     with open(file_path, 'r') as file:
@@ -123,20 +170,80 @@ def parse_tokens(file_path):
     
     return tokens
 
-if __name__ == '__main__':
-    tokens = parse_tokens('src.sc')
-    for tok in tokens:
-        if tok.type == TOKEN_KEYWORD:
-            tokstr = 'KEYWORD'
-        elif tok.type == TOKEN_SYMBOL:
-            tokstr = 'SYMBOL'
-        elif tok.type == TOKEN_IDENTIFIER:
-            tokstr = 'IDENTIFIER'
-        elif tok.type == TOKEN_NUMBER:
-            tokstr = 'NUMBER'
-        elif tok.type == TOKEN_STRING:
-            tokstr = 'STRING'
-        else:
-            tokstr = '[ERROR]'
+def parse_function(program, name, func_type, tokens):
+    while not tokens.pop().is_keyword('end'): pass
+    return {
+        'name': name,
+        'type': func_type
+    }
+
+def parse_program(tokens):
+    program = {}
+    program['costumes'] = []
+    program['sounds'] = []
+    program['functions'] = {}
+    program['events'] = {}
+
+    KEYWORD_TYPES = ['void', 'string', 'number']
+    EVENT_NAMES = ['flag', 'keypress', 'click', 'broadcast']
+
+    while tokens:
+        tok = tokens.pop()
+
+        # costumes
+        # syntax: costume <str1> [, [...]]
+        if tok.is_keyword('costume'):
+            asset_name = tokens.pop()
+            assert(asset_name.type == TOKEN_STRING)
+            program['costumes'].append(asset_name.value)
         
-        print(f"{tokstr}: {(tok.value)}")
+        # sounds
+        # syntax similar to costumes
+        elif tok.is_keyword('sound'):
+            asset_name = tokens.pop()
+            assert(asset_name.type == TOKEN_STRING)
+            program['sounds'].append(asset_name.value)
+        
+        # events
+        # syntax: on <event_name>: <func1> [, [...]]
+        elif tok.is_keyword('on'):
+            event_name = tokens.pop().get_identifier()
+            assert(event_name in EVENT_NAMES)
+            assert(tokens.pop().is_symbol(':'))
+
+            if not event_name in program['events']:
+                program['events'][event_name] = []
+            event_targets = program['events'][event_name]
+
+            fname = tokens.pop().get_identifier()
+            assert(fname in program['functions'])
+
+            if not fname in event_targets:
+                event_targets.append(fname)
+
+            program['events'][event_name] = event_targets
+        
+        # function definition
+        elif tok.is_keyword('func'):
+            func_name = tokens.pop().get_identifier()
+            assert(not func_name in program['functions'])
+            assert(tokens.pop().is_symbol('('))
+            assert(tokens.pop().is_symbol(')'))
+            assert(tokens.pop().is_symbol(':'))
+            func_type = tokens.pop()
+            assert(func_type.type == TOKEN_KEYWORD and func_type.value in KEYWORD_TYPES)
+
+            program['functions'][func_name] = parse_function(program, func_name, func_type.value, tokens)
+        
+        else:
+            raise Exception("unknown token " + str(tok))
+    
+    return program
+
+def main():
+    tokens = TokenQueue(parse_tokens('src.sc'))
+    program = parse_program(tokens)
+    print(program)
+
+if __name__ == '__main__':
+    main()    
